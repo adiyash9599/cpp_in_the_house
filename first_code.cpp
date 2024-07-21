6310e625108c417d4498b0936dc1b872e5675543
adiyash9599 <aditya9599chauhan@gmail.com>
2024-07-22 01:51:57 +0530

    Variables and Datatypes

---
+++ first_code.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Ajay Pratap Singh\n"
+         << "Seema Chauhan\n"
+         << "Kirti Chauhan\n"
+         << "Aditya Chauhan\n";
+    // cout << "Seema Chauhan" << endl;
+    // cout << "Kirti Chauhan" << endl;
+    // cout << "Aditya Chauhan" << endl;
+    return 0;
+}
