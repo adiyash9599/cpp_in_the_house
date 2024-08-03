ab557e124f1a9d1951d1f18fc94da6efe2e312e7
adiyash9599 <aditya9599chauhan@gmail.com>
2024-08-03 10:02:29 +0530

    variables and datatypes pt.2

---
+++ lecture.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <iomanip>
+#define PI 3.14
+using namespace std;
+
+int main()
+{
+    // int a;
+    // cout << a << endl;
+    /*cout << sizeof(a) << endl;*/
+
+    // float cgpa = 7.95;
+    // double cgpa2 = 7.956;
+
+    // cout << setprecision(12) << cgpa << endl;
+    // cout << setprecision(12) << cgpa2 << endl;
+
+    // int age;
+    // cout << "Enter your age: ";
+
+    // cin >> age;
+    // cout << "Your age is: " << age << endl;
+
+    // average
+    // float eng;
+    // float math;
+    // float sci;
+
+    // cout << "Enter your english marks: ";
+    // cin >> eng;
+    // cout << "Enter your math marks: ";
+    // cin >> math;
+    // cout << "Enter your science marks: ";
+    // cin >> sci;
+
+    // float average;
+    // average = (eng + math + sci) / 3;
+    // cout << "Your average marks is: " << average << endl;
+
+    // area of square
+    // int side;
+    // cout << "Enter the side of the square: ";
+    // cin >> side;
+    // cout << "The area of the square is: ";
+    // cout << side * side;
+
+    // total cost with gst
+    // float item1, item2, item3;
+    // cout << "Enter the price of item 1: " << endl;
+    // cin >> item1;
+    // cout << "Enter the price of item 2: " << endl;
+    // cin >> item2;
+    // cout << "Enter the price of item 3: " << endl;
+    // cin >> item3;
+
+    // float total = item1 + item2 + item3;
+    // float totalWithGST = total + (total * 18 / 100);
+    // cout << "Total price of the items is: " << totalWithGST << endl;
+
+    // simple interest
+    // float p, r, t;
+
+    // cout << "Enter the principal amount: ";
+    // cin >> p;
+    // cout << "Enter the rate of interest: ";
+    // cin >> r;
+    // cout << "Enter the time period: ";
+    // cin >> t;
+
+    // float si = (p * r * t) / 100;
+    // cout << "The simple interest is: " << si << endl;
+
+    // radius of a circle
+    // float r;
+    // cout << "Enter the radius of the circle: ";
+    // cin >> r;
+
+    // float area = PI * r * r;
+    // cout << "The area of the circle is: " << area << endl;
+    return 0;
+}
